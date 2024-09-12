58b7c1879a857a961466f803c4f4e56b85c223d3
Rodrigo Kern <rodrigokern@gmail.com>
2024-09-12 13:54:49 -0300

    Desafio concluido

---
+++ db.sql
@@ -0,0 +1,26 @@
+﻿USE master
+GO
+IF NOT EXISTS (
+    SELECT name
+        FROM sys.databases
+        WHERE name = N'TrilhaApiDesafio'
+)
+CREATE DATABASE TrilhaApiDesafio
+GO
+
+USE TrilhaApiDesafio
+
+-- Drop the table if it already exists
+IF OBJECT_ID('dbo.Tarefas', 'U') IS NOT NULL
+DROP TABLE dbo.Tarefas
+GO
+
+CREATE TABLE dbo.Tarefas
+(
+    id INT NOT NULL IDENTITY(1,1) PRIMARY KEY, -- primary key column
+    titulo [NVARCHAR](200) NULL,
+    descricao [NVARCHAR](200) NULL,
+    [data] datetime NOT NULL,
+    [status] INT NOT NULL
+);
+GO
